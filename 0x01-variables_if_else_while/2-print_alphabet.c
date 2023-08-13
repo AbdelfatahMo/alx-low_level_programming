aae4ee67cdb358e0286ebd7d04f0dcf7ada1b7a9
AbdelfatahMo <b5b71999@outlook.com>
2023-08-13 23:20:53 +0300

    0x01 2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+for (char i = 'a'; i <= 'z'; i++)
+{
+putchar(i);
+}
+return (0);
+}
