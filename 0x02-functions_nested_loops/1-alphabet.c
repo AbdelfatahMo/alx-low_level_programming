45aba06f56d5ec2aca021471d599a63994534c2a
AbdelfatahMo <b5b71999@outlook.com>
2023-08-15 14:39:28 +0300

    0x02 1-alphabet

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -9,11 +9,16 @@
  */
 int main(void)
 {
+print_alphabet();
+return (0);
+}
+
+void print_alphabet(void)
+{
 char i;
 for (i = 'a'; i <= 'z'; i++)
 {
 _putchar(i);
 }
 _putchar('\n');
-return (0);
 }
