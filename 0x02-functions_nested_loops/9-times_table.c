0f91670d118bfe270368fb7db1c6267401a523f0
AbdelfatahMo <b5b71999@outlook.com>
2023-08-15 17:51:26 +0300

    0x02 9-times_table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -13,9 +13,12 @@ for (h = 0; h < 10; h++)
 {
 for (h1 = 0; h1 < 10; h1++)
 {
-_putchar((h + 48) * h1);
+_putchar((h * h1) + 48);
+if (h1 < 9)
+{
 _putchar(',');
 _putchar(' ');
+}
 }
 _putchar('\n');
 }
