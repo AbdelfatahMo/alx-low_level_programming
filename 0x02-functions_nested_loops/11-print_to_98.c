8ba894a2252a1ca734838e3428eb9d445f80103d
AbdelfatahMo <b5b71999@outlook.com>
2023-08-16 01:14:05 +0300

    0x02 11-print_to_98

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,54 @@
+#include "main.h"
+/**
+ * print_to_98 - prints day minutes
+ * @a: a  are int arg
+ * Return: sum of a,b
+ * On error, returnnothing.
+ */
+void print_to_98(int a)
+{
+if (a > 98)
+{
+for (int i = a; i < 97; i--)
+{
+_putchar((i % 10) + 48);
+if (i % 100 !=0)
+{
+_putchar((i % 100) - (i % 10) + 48);
+}
+if (i % 1000 !=0)
+{
+_putchar((i % 1000) - (i % 100) - (i % 10) + 48);
+}
+if (i != 98)
+{
+_putchar(' ');
+_putchar(',');
+}
+}
+}
+else
+{
+for (int i = a; i < 99; i++)
+{
+if (a < 0)
+{
+_putchar('-');
+}
+_putchar((i % 10) + 48);
+if (i % 100 !=0)
+{
+_putchar((i % 100) - (i % 10) + 48);
+}
+if (i % 1000 !=0)
+{
+_putchar((i % 1000) - (i % 100) - (i % 10) + 48);
+}
+if (i != 98)
+{
+_putchar(' ');
+_putchar(',');
+}
+}   
+}
+}
