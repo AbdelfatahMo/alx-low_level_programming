da81cb256b4ec620e28f424e622aa92dfb010a06
AbdelfatahMo <b5b71999@outlook.com>
2023-08-15 15:57:39 +0300

    0x02 7-print_last_digit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * print_last_digit - returns last digit
+ * @a: The int to check
+ * Return: void
+ * On error, returnnothing.
+ */
+int print_last_digit(int a)
+{
+return (a%10);
+}
