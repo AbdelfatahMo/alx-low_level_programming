cc58c7059f3970065026b349ad98b80585121a30
AbdelfatahMo <b5b71999@outlook.com>
2023-08-15 15:44:59 +0300

    0x02 6-abs.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,11 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * _abs - returns absolute value
+ * @a: The int to check
+ * Return: void
+ * On error, returnnothing.
+ */
+int _abs(int a) {
+return (a < 0) ? -a : a;
+}
