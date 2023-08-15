9363317350b6aa4dbfd3dc3c752314dfbc408315
AbdelfatahMo <b5b71999@outlook.com>
2023-08-15 15:01:10 +0300

    0x02 3-islower

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _islower - returns 1 if is lower case else 1
+ *
+ * Return: void
+ * On error, returnnothing.
+ */
+int _islower(int c)
+{
+if (c >= 65 && c <= 90 )
+{
+return (0);
+}
+else
+{
+return (1);
+}
+}
