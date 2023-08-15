59d9e2296e871403d3db0d25d5abebbd95236ff2
AbdelfatahMo <b5b71999@outlook.com>
2023-08-15 16:41:27 +0300

    0x02 8-24_hours

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * jack_bauer - 
+ * @a: The int to check
+ * Return: void
+ * On error, returnnothing.
+ */
+void jack_bauer(void)
+{
+for (int h = 0; h < 3; h++)
+{
+for (int h1 = 0; h1 < 10; h1++)
+{
+for (int m = 0; m < 6; m++)
+{
+for (int m1 = 0; m1 < 10; m1++)
+{  
+_putchar(h);
+_putchar(h1);
+_putchar(':');
+_putchar(m);
+_putchar(m1);
+_putchar('\n');                
+}            
+}        
+}    
+}
+}
