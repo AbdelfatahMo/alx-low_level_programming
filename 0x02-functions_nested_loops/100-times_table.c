f2a93a2fc83e4fa39440561f1d0631ffdb51801f
AbdelfatahMo <b5b71999@outlook.com>
2023-08-16 12:47:23 +0300

    0x02 100-times_table

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -7,33 +7,36 @@
  */
 void print_times_table(int n)
 {
-    int num, y, i, o;
+    int num, y, i, o, x;
     if (n <= 15 && n >= 0)
     {
         for (i = 0; i <= n + 1; i++)
         {
             for (o = 0; o <= n; o++)
-            {
+            {   
                 num = i * o;
-                y = 3;
-                (num >= 100) ? _putchar(((num - ((num % 100))) / 100) + 48) : y--;
-                (num >= 10) ? _putchar(((num - (num % 10)) / 10) + 48) : y--;
+                (num >= 100) ? _putchar(((num - ((num % 100))) / 100) + 48) : y;
+                (num >= 10) ? _putchar(((num - (num % 10)) / 10) + 48) : y ;
                 _putchar((num % 10) + 48);
                 _putchar(',');
                 if (i * (o + 1) > 99)
                 {
-                    _putchar(' ');
+                    y = 1;
                 }
                 else if (i * (o + 1) > 9)
                 {
+                    y = 2;
                 }
                 else
                 {
-                    _putchar(' ');
-                    _putchar(' ');
+                    y = 3;
+                }
+                for ( x = 0; x <= y; y++)
+                {
                     _putchar(' ');
                 }
             }
+            _putchar('\n');
         }
     }
 }
