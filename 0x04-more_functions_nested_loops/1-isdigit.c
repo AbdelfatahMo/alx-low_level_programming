d50598e7e2aeb1053950ac7f3fa09299825a35b9
AbdelfatahMo <b5b71999@outlook.com>
2023-08-17 11:35:08 +0300

    0x04 0-isdigit

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _isalpha - returns 1 if is alpha case else 1
+ * @c: The int to check
+ * Return: void
+ * On error, returnnothing.
+ */
+int _isupper(int c)
+{
+
+if (c >= 48 && c <= 57)
+{
+return (1);
+}
+else
+{
+return (0);
+}
+}
