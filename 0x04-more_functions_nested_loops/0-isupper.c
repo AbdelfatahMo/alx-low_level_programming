5e4806ad79971689fc8ab0bda40ce38450d648da
AbdelfatahMo <b5b71999@outlook.com>
2023-08-17 11:31:35 +0300

    0x04 0-isupper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _isalpha - returns 1 if is alpha case else 1
+ * @c: The int to check
+ * Return: void
+ * On error, returnnothing.
+ */
+int _isupper(int c)
+{
+
+if (c >= 65 && c <= 90)
+{
+return (1);
+}
+else if (c >= 97 && c <= 122)
+{
+return (0);
+}
+}
