21f7842f429ceb5f1ac7619fca5d9584c3139057
AbdelfatahMo <b5b71999@outlook.com>
2023-08-18 04:40:18 +0300

    0x04

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -7,43 +7,28 @@
 int main(void)
 {
     int i, c;
-    char ch[] = {'F', 'i', 'z', 'z', 'B', 'u', 'z', 'z'};
+
     for (i = 1; i <= 100; i++)
     {
         if (i % 3 == 0 && i % 5 == 0)
         {
-            for (c = 0; c < 8; c++)
-            {
-                _putchar(ch[c]);
-            }
-            _putchar(' ');
+            ch(0, 8);
         }
         else if (i % 3 == 0)
         {
-            for (c = 0; c < 4; c++)
-            {
-                _putchar(ch[c]);
-            }
-            _putchar(' ');
+            ch(0, 4);
         }
         else if (i % 5 == 0)
         {
-            for (c = 4; c < 8; c++)
-            {
-                _putchar(ch[c]);
-            }
-            _putchar(' ');
-        }
-        else if (i > 9)
-        {
-            _putchar(((i - (i % 10)) / 10) + 48);
-            _putchar((i % 10) + 48);
-            _putchar(' ');
+            ch(4, 8);
         }
         else
         {
-            _putchar(i + 48);
-            _putchar(' ');
+            if (i > 9)
+            {
+                _putchar(((i - (i % 10)) / 10) + 48);
+            }
+            _putchar((i % 10) + 48);
         }
         _putchar(' ');
     }
---
+++ 0x04-more_functions_nested_loops/func.c
@@ -0,0 +1,9 @@
+void ch(int start, int last)
+{
+    int c;
+    char ch[] = {'F', 'i', 'z', 'z', 'B', 'u', 'z', 'z'};
+    for (c = start; c < last; c++)
+    {
+        _putchar(ch[c]);
+    }
+}
