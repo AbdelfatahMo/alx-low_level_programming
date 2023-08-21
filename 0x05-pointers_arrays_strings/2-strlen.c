fee128e702b0ecab230fc2a2785d3b253d77c0df
AbdelfatahMo <b5b71999@outlook.com>
2023-08-22 00:06:47 +0300

    0x05 2-strlen

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -11,14 +11,14 @@ int i;
 i = 0;
 while (1)
 {
-    if (i == 0 && *s == '\0')
-    {
-     return (0);   
-    }
-    else if (*(s+i)== '\0')
-    {
-        return (i);
-    }
-    i++;
+if (i == 0 && *s == '\0')
+{
+return (0);   
+}
+else if (*(s + i)== '\0')
+{
+return (i);
+}
+i++;
 }
 }
