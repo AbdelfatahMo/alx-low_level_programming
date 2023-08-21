417287d0b6d676125e487802e841880887fd8c45
AbdelfatahMo <b5b71999@outlook.com>
2023-08-22 00:32:07 +0300

    0x05 4-print_rev

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * print_rev - print
+ * @s : pointer of string
+ * Return: int
+ * On error, returnnothing.
+ */
+void print_rev(char *s)
+{
+int i = sizeof(s);
+char c;
+i = 0;
+while (1)
+{
+if (i == 0)
+{
+c = *s;
+_putchar(c);
+_putchar('\n');
+break;
+}
+else if (*(s + i) != '\0')
+{
+c = *(s + i);
+_putchar(c);
+}
+i--;
+}
+}
