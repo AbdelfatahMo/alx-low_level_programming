48f7ea88f9be9393d97a76c41bae4cdc1b83dd14
AbdelfatahMo <b5b71999@outlook.com>
2023-08-22 01:24:14 +0300

    0x05 4-print_rev

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -7,34 +7,34 @@
  */
 void print_rev(char *s)
 {
-    int i = 0;
-    while (1)
-    {
-        if (i == 0 && *s == '\0')
-        {
-            break;
-        }
-        else if (*(s + i) == '\0')
-        {
-            break;
-        }
-        i++;
-    }
-    while (1)
-    {   
-        i--;
-        if (i == 0)
-        {
-            _putchar(*s);
-        }
-        else if (i > 0)
-        {
-            _putchar(*(s + i));
-        }
-        else
-        {
-            _putchar('\n');
-            break;
-        }
-    }
+int i = 0;
+while (1)
+{
+if (i == 0 && *s == '\0')
+{
+break;
+}
+else if (*(s + i) == '\0')
+{
+break;
+}
+i++;
+}
+while (1)
+{   
+i--;
+if (i == 0)
+{
+_putchar(*s);
+}
+else if (i > 0)
+{
+_putchar(*(s + i));
+}
+else
+{
+_putchar('\n');
+break;
+}
+}
 }
