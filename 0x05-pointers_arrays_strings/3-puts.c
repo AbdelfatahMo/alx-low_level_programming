b9c8f7defd7da7e256dee858cf720c00d489040c
AbdelfatahMo <b5b71999@outlook.com>
2023-08-22 00:15:01 +0300

    0x05 3-puts

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+ * _strlen - swap
+ * @s : The int pointer to swap
+ * Return: int
+ * On error, returnnothing.
+ */
+void _puts(char *str)
+{
+int i;
+char c;
+i = 0;
+while (1)
+{
+if (i == 0 && *str != '\0')
+{
+c = *str;
+_putchar(c);
+}
+else if (*(str + i) == '\0')
+{
+c = *(str + 1);
+_putchar(c);
+}else
+{
+_putchar('\n');
+}
+i++;
+}
+}
