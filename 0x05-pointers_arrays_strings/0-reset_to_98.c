659da20f28c214dad12accc6fc57b9160efaf19d
AbdelfatahMo <b5b71999@outlook.com>
2023-08-21 23:37:51 +0300

    0x05 0-rest_to_98

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * more_numbers - returns 1 if is alpha case else 1
+ * @*a The int to check
+ * Return: void
+ * On error, returnnothing.
+ */
+void reset_to_98(int *n)
+{
+*n = 98;
+}
