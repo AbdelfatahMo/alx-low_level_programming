12dfc21a00920277f6b32d58d6c7040ed8d89856
AbdelfatahMo <b5b71999@outlook.com>
2023-08-22 01:48:37 +0300

    0x05 5-rev_string

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -7,34 +7,10 @@
  */
 void rev_string(char *s)
 {
-int i = 0;
-while (1)
+char c;
+int i = _strlen(s);
+for (int x = 0; x < (i / 2) -1; x++)
 {
-if (i == 0 && *s == '\0')
-{
-break;
-}
-else if (*(s + i) == '\0')
-{
-break;
-}
-i++;
-}
-while (1)
-{
-i--;
-if (i == 0)
-{
-_putchar(*s);
-}
-else if (i > 0)
-{
-_putchar(*(s + i));
-}
-else
-{
-_putchar('\n');
-break;
-}
+    swap_int(*(s + x),*(s + (i - x)));
 }
 }
