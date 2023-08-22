180f2b2adcab03c988ae0a42b363104e35e8fd08
AbdelfatahMo <b5b71999@outlook.com>
2023-08-23 00:05:23 +0300

    0x05 6-puts2

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -7,28 +7,28 @@
  */
 void puts_half(char *str)
 {
-    int i,x,y;
-    i = 0;
-    while (1)
-    {
-        if (i == 0 && *str == '\0')
-        {
-            break;
-        }
-        else if (*(str + i) == '\0')
-        {
-            break;
-        }
-        i++;
-    }
-    x = (i + 1) / 2;
-    if (i % 2 == 1)
-    {
-        x = (i + 2) / 2;
-    }
-    for (y = x; y <= i; y++)
-    {
-        _putchar(*(str + y));
-    }
-    _putchar('\n');
+int i,x,y;
+i = 0;
+while (1)
+{
+if (i == 0 && *str == '\0')
+{
+break;
+}
+else if (*(str + i) == '\0')
+{
+break;
+}
+i++;
+}
+x = (i + 1) / 2;
+if (i % 2 == 1)
+{
+x = (i + 2) / 2;
+}
+for (y = x; y <= i; y++)
+{
+_putchar(*(str + y));
+}
+_putchar('\n');
 }
