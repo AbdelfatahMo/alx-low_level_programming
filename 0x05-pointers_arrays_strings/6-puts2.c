c813b6df61e73fee472bb06b80f47a049e3fd2c4
AbdelfatahMo <b5b71999@outlook.com>
2023-08-22 23:03:29 +0300

    0x05 6-puts2

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -7,24 +7,24 @@
  */
 void puts2(char *str)
 {
-    int i;
-    char c;
-    i = 0;
-    while (1)
-    {
-        if (*(str + i) == '\0')
-        {
-        	break;
-        }
-        else if (i % 2 == 0)
-        {
-            c = *(str + i);
-            _putchar(c);
-        }
-        else
-        {
-        }
-        i++;
-    }
-    _putchar('\n');
+int i;
+char c;
+i = 0;
+while (1)
+{
+if (*(str + i) == '\0')
+{
+break;
+}
+else if (i % 2 == 0)
+{
+c = *(str + i);
+_putchar(c);
+}
+else
+{
+}
+i++;
+}
+_putchar('\n');
 }
