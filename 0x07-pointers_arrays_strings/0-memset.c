4202ed8b3c9572c4f898cdcd71d12d1c71a03dc0
AbdelfatahMo <b5b71999@outlook.com>
2023-08-29 10:05:38 +0300

    0x07 0-memset

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _memset - return
+ * @s : string
+ * @b : char
+ * @n : positive int
+ * Return: void
+ * On error, returnnothing.
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+int i;
+for (i = 0; i < n; i++)
+{
+*(s + i) = b;
+}
+}
