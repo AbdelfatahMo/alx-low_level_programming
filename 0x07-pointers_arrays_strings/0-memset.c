93cbc565ef64aa670f12054340c187b0d9923334
AbdelfatahMo <b5b71999@outlook.com>
2023-08-29 10:26:40 +0300

    0x07 0-memset

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -9,10 +9,24 @@
  */
 char *_memset(char *s, char b, unsigned int n)
 {
-int i;
+int i = 0;
+while (*(s + i) != '\0')
+{
+if (i % 10 == 0)
+{
+_putchar('\n');
+}   
+_putchar(*s + i);
+}
+_putchar('\n');
 for (i = 0; i < n; i++)
 {
 *(s + i) = b;
+if (i % 10 == 0)
+{
+_putchar('\n');
+}   
+_putchar(*s + i);
 }
 return s;
 }
